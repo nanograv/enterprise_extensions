4c215c8109f9a6f6393b2c53f04776e07150fbf3
Hazboun6 <jeffrey.hazboun@gmail.com>
2020-02-20 21:06:18 -0800

    Fixed new kwarg calls in chromatic noise models

--- tests/test_models.py
+++ tests/test_models.py
@@ -60,14 +60,14 @@ def test_model_singlepsr_noise_sw(nodmx_psrs,caplog):
 
 def test_model_singlepsr_noise_chrom_nondiag(nodmx_psrs,caplog):
     # caplog.set_level(logging.CRITICAL)
-    m=models.model_singlepsr_noise(nodmx_psrs[1], chrom=True)
+    m=models.model_singlepsr_noise(nodmx_psrs[1], chrom_gp=True)
     assert hasattr(m,'get_lnlikelihood')
     x0 = {pname:p.sample() for pname,p in zip(m.param_names, m.params)}
     m.get_lnlikelihood(x0)
 
 def test_model_singlepsr_noise_chrom_diag(nodmx_psrs,caplog):
     # caplog.set_level(logging.CRITICAL)
-    m=models.model_singlepsr_noise(nodmx_psrs[1], chrom=True,
+    m=models.model_singlepsr_noise(nodmx_psrs[1], chrom_gp=True,
                                    chrom_gp_kernel='diag')
     assert hasattr(m,'get_lnlikelihood')
     x0 = {pname:p.sample() for pname,p in zip(m.param_names, m.params)}
