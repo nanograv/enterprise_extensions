7393f9d0c6c52de794730bbb2e687e7820efd8aa
Andrew Kaiser <ark0015@mix.wvu.edu>
2020-04-04 11:49:10 -0400

    Making timing more user/output friendly with naming and sampling.

--- enterprise_extensions/timing.py
+++ enterprise_extensions/timing.py
@@ -43,24 +43,32 @@ def BoundedNormal(mu=0, sigma=1, pmin=-1, pmax=1, size=None):
     return BoundedNormal
 
 
-def get_default_physical_tm_priors():
+def get_prior(
+    prior_type, prior_sigma, prior_lower_bound, prior_upper_bound, num_params=None
+):
     """
-    "RAJ", "DECJ", "ELONG", "ELAT", "BETA", "LAMBDA", "PX"
-    "PMDEC", "PMRA", "PMELONG", "PMELAT", "PMRV", "PMBETA", "PMLAMBDA"
-    "F", "F0", "F1", "F2", "P", "P1","PB","T0","A1","OM","EPS1","EPS2",
-    "EPS1DOT","EPS2DOT","FB","MTOT","M2","XDOT","X2DOT","EDOT","H3",
-    "H4","OMDOT","OM2DOT","XOMDOT","PBDOT","XPBDOT","GAMMA","PPNGAMMA",
-    "DR","DTHETA"
+    Returns the requested prior for a parameter
+    
+    :param prior_type: prior on timing parameters.
+    :param prior_sigma: Sets the sigma on timing parameters for normal distribution draws
+    :param prior_lower_bound: Sets the lower bound on timing parameters for bounded normal and uniform distribution draws
+    :param prior_upper_bound: Sets the upper bound on timing parameters for bounded normal and uniform distribution draws
+    :param num_params: number of timing parameters assigned to prior. Default is None (ie. only one)
     """
-    default_tm_priors = {}
-    default_tm_priors["E"] = {"pmin": 0.0, "pmax": 1.0}
-    default_tm_priors["ECC"] = {"pmin": 0.0, "pmax": 1.0}
-    default_tm_priors["SINI"] = {"pmin": 0.0, "pmax": 1.0}
-    return
-
-
-def get_astrometric_priors():
-    astrometric_priors = {}
+    if prior_type == "bounded-normal":
+        return BoundedNormal(
+            mu=0.0,
+            sigma=prior_sigma,
+            pmin=prior_lower_bound,
+            pmax=prior_upper_bound,
+            size=num_params,
+        )
+    elif prior_type == "uniform":
+        return parameter.Uniform(prior_lower_bound, prior_upper_bound, size=num_params)
+    else:
+        raise ValueError(
+            "prior_type can only be uniform or bounded-normal, not ", prior_type
+        )
 
 
 def get_par_errors(t2psr, par):
@@ -71,22 +79,23 @@ def get_par_errors(t2psr, par):
     :param par: parameter to pull error from par file
     """
     filename = t2psr.parfile.split("/")[-1]
-    file = glob.glob("../../*/*/" + filename)[0]
+    file = glob.glob("../../*/par/" + filename)[0]
+
     with open(file, "r") as f:
         for line in f.readlines():
             if par == "ELONG":
                 # enterprise renames LAMBDA
                 if line.split()[0] in [par, "LAMBDA"]:
-                    error = line.split()[-1]
+                    error = ephem.degrees(line.split()[-1])
                     return error
             elif par == "ELAT":
                 # enterprise renames BETA
                 if line.split()[0] in [par, "BETA"]:
-                    error = line.split()[-1]
+                    error = ephem.degrees(line.split()[-1])
                     return error
             else:
                 if line.split()[0] == par:
-                    error = line.split()[-1]
+                    error = ephem.degrees(line.split()[-1])
                     return error
                 else:
                     raise ValueError(par, " not in file!")
@@ -94,16 +103,7 @@ def get_par_errors(t2psr, par):
 
 # timing model delay
 @signal_base.function
-def tm_delay(
-    t2pulsar,
-    tmparams_orig,
-    param_dict,
-    pos_params,
-    pm_params,
-    spin_params,
-    kep_params,
-    gr_params,
-):
+def tm_delay(t2pulsar, tmparams_orig, **kwargs):
     """
     Compute difference in residuals due to perturbed timing model.
 
@@ -122,96 +122,47 @@ def tm_delay(
     Feed the priors and param list into tm_delay function
     """
     residuals = t2pulsar.residuals()
-
     # grab original timing model parameters and errors in dictionary
     orig_params = {}
     tm_params_rescaled = {}
     error_pos = {}
-    for tm_category, tm_param_keys in param_dict.items():
-        if tm_category == "pos":
-            normed_params = pos_params
-        elif tm_category == "pm":
-            normed_params = pm_params
-        elif tm_category == "spin":
-            normed_params = spin_params
-        elif tm_category == "kep":
-            normed_params = kep_params
-        elif tm_category == "gr":
-            normed_params = gr_params
+    for tm_scaled_key, tm_scaled_val in kwargs.items():
+        tm_param = tm_scaled_key.split("_")[-1]
+        orig_params[tm_param] = tmparams_orig[tm_param][0]
+        # Section because there are incorrect handlings of errors for ecliptic coordinates, idk why
+        if tm_param in ["ELONG", "LAMBDA"]:
+            error_pos["ELONG"] = get_par_errors(t2pulsar, tm_param)
+        elif tm_param in ["ELAT", "BETA"]:
+            error_pos["ELAT"] = get_par_errors(t2pulsar, tm_param)
+
+        if tm_param in ["ELONG", "LAMBDA", "ELAT", "BETA"] and error_pos.keys() >= {
+            "ELAT",
+            "ELONG",
+        }:
+            ec_errors = ephem.Ecliptic(
+                error_pos["ELONG"]["err"], error_pos["ELAT"]["err"]
+            )
+
+            tm_params_rescaled["ELONG"] = (
+                tm_scaled_val * np.double(ec_errors.lon) + tmparams_orig["ELONG"][0]
+            )
+            tm_params_rescaled["ELAT"] = (
+                tm_scaled_val * np.double(ec_errors.lat) + tmparams_orig["ELAT"][0]
+            )
+
         else:
-            normed_params = None
-
-        for i, tm_param in enumerate(tm_param_keys):
-            orig_params[tm_param] = tmparams_orig[tm_param][0]
-            # Section because there are incorrect handlings of errors for ecliptic coordinates, idk why
-            if tm_param in ["ELONG", "LAMBDA"]:
-                error_pos["ELONG"] = {
-                    "err": get_par_errors(t2pulsar, tm_param),
-                    "param_iter": i,
-                }
-            elif tm_param in ["ELAT", "BETA"]:
-                error_pos["ELAT"] = {
-                    "err": get_par_errors(t2pulsar, tm_param),
-                    "param_iter": i,
-                }
-
-            if tm_param in ["ELONG", "LAMBDA", "ELAT", "BETA"] and error_pos.keys() >= {
-                "ELAT",
-                "ELONG",
-            }:
-                ec_errors = ephem.Ecliptic(
-                    error_pos["ELONG"]["err"], error_pos["ELAT"]["err"]
-                )
-
-                tm_params_rescaled["ELONG"] = (
-                    normed_params[error_pos["ELONG"]["param_iter"]]
-                    * np.double(ec_errors.lon)
-                    + tmparams_orig["ELONG"][0]
-                )
-                tm_params_rescaled["ELAT"] = (
-                    normed_params[error_pos["ELAT"]["param_iter"]]
-                    * np.double(ec_errors.lat)
-                    + tmparams_orig["ELAT"][0]
-                )
-                # End of handling section
-                """
-                for key in error_pos.keys():
-                    print(key,': ')
-                    print(' Original Value: ', orig_params[key])
-                    print(' normed_params: ', normed_params[error_pos[key]["param_iter"]])
-                    if key == "ELONG":
-                        print(' tmparam Errors: ',np.double(ec_errors.lon))
-                    else:
-                        print(' tmparam Errors: ',np.double(ec_errors.lat))
-                    print(' tmparam Value: ',tmparams_orig[key][0])
-                    print(' New Value: ',tm_params_rescaled[key])
-                """
-            else:
-                if isinstance(normed_params, (list, np.ndarray)):
-                    tm_params_rescaled[tm_param] = (
-                        normed_params[i] * tmparams_orig[tm_param][1]
-                        + tmparams_orig[tm_param][0]
-                    )
-                elif isinstance(normed_params, (float, int)):
-                    tm_params_rescaled[tm_param] = (
-                        normed_params * tmparams_orig[tm_param][1]
-                        + tmparams_orig[tm_param][0]
-                    )
-                # Making sanity checks
-                if tm_param in ["E", "ECC"]:  # ,"SINI"]:
-                    if tm_params_rescaled[tm_param] <= 0.0:
-                        tm_params_rescaled[tm_param] = 1e-9
-                    elif tm_params_rescaled[tm_param] >= 1.0:
-                        tm_params_rescaled[tm_param] = 1.0 - 1e-9
-                """
-                if tm_param not in ["ELONG","ELAT"]:
-                    print(tm_param,': ')
-                    print(' Original Value: ', orig_params[tm_param])
-                    print(' normed_params: ', normed_params)
-                    print(' tmparam Errors: ',tmparams_orig[tm_param][1])
-                    print(' tmparam Value: ',tmparams_orig[tm_param][0])
-                    print(' New Value: ',tm_params_rescaled[tm_param])
-                """
+            tm_params_rescaled[tm_param] = (
+                tm_scaled_val * tmparams_orig[tm_param][1] + tmparams_orig[tm_param][0]
+            )
+            # Making sanity checks
+            if tm_param in ["E", "ECC"]:  # ,"SINI"]:
+                if tm_params_rescaled[tm_param] <= 0.0:
+                    tm_params_rescaled[tm_param] = 1e-9
+                elif tm_params_rescaled[tm_param] >= 1.0:
+                    tm_params_rescaled[tm_param] = 1.0 - 1e-9
+            if tm_param in ["PX","M2"]:
+                if tm_params_rescaled[tm_param] <= 0.0:
+                    tm_params_rescaled[tm_param] = 1e-9
 
     # set to new values
     t2pulsar.vals(tm_params_rescaled)
@@ -243,10 +194,14 @@ def timing_block(
     :param prior_lower_bound: Sets the lower bound on timing parameters for bounded normal and uniform distribution draws
     :param prior_upper_bound: Sets the upper bound on timing parameters for bounded normal and uniform distribution draws
     """
-    param_dict = defaultdict(list)
+    tm_delay_kwargs = {}
     for par in tmparam_list:
         if par in ["RAJ", "DECJ", "ELONG", "ELAT", "BETA", "LAMBDA", "PX"]:
-            param_dict["pos"].append(par)
+            key_string = "pos_param_" + par
+            tm_delay_kwargs[key_string] = get_prior(
+                prior_type, prior_sigma, prior_lower_bound, prior_upper_bound
+            )
+
         elif par in [
             "PMDEC",
             "PMRA",
@@ -256,9 +211,15 @@ def timing_block(
             "PMBETA",
             "PMLAMBDA",
         ]:
-            param_dict["pm"].append(par)
+            key_string = "pm_param_" + par
+            tm_delay_kwargs[key_string] = get_prior(
+                prior_type, prior_sigma, prior_lower_bound, prior_upper_bound
+            )
         elif par in ["F", "F0", "F1", "F2", "P", "P1"]:
-            param_dict["spin"].append(par)
+            key_string = "spin_param_" + par
+            tm_delay_kwargs[key_string] = get_prior(
+                prior_type, prior_sigma, prior_lower_bound, prior_upper_bound
+            )
         elif par in [
             "PB",
             "T0",
@@ -278,7 +239,10 @@ def timing_block(
             "X2DOT",
             "EDOT",
         ]:
-            param_dict["kep"].append(par)
+            key_string = "kep_param_" + par
+            tm_delay_kwargs[key_string] = get_prior(
+                prior_type, prior_sigma, prior_lower_bound, prior_upper_bound
+            )
         elif par in [
             "H3",
             "H4",
@@ -292,121 +256,21 @@ def timing_block(
             "DR",
             "DTHETA",
         ]:
-            param_dict["gr"].append(par)
+            key_string = "gr_param_" + par
+            tm_delay_kwargs[key_string] = get_prior(
+                prior_type, prior_sigma, prior_lower_bound, prior_upper_bound
+            )
         else:
             if "DMX" in ["".join(list(x)[0:3]) for x in par.split("_")][0]:
-                param_dict["dmx"].append(par)
+                key_string = "dmx_param_" + par
+                tm_delay_kwargs[key_string] = get_prior(
+                    prior_type, prior_sigma, prior_lower_bound, prior_upper_bound
+                )
             else:
                 print(par, " is not currently a modelled parameter.")
 
-    # default 2-sigma prior above and below the parfile mean
-    if len(param_dict["pos"]) != 0:
-        if prior_type == "bounded-normal":
-            pos_params = BoundedNormal(
-                mu=0.0,
-                sigma=prior_sigma,
-                pmin=prior_lower_bound,
-                pmax=prior_upper_bound,
-                size=len(param_dict["pos"]),
-            )
-        elif prior_type == "uniform":
-            pos_params = parameter.Uniform(
-                prior_lower_bound, prior_upper_bound, size=len(param_dict["pos"])
-            )
-        else:
-            raise ValueError(
-                "prior_type can only be uniform or bounded-normal, not ", prior_type
-            )
-    else:
-        pos_params = None
-
-    if len(param_dict["pm"]) != 0:
-        if prior_type == "bounded-normal":
-            pm_params = BoundedNormal(
-                mu=0.0,
-                sigma=prior_sigma,
-                pmin=prior_lower_bound,
-                pmax=prior_upper_bound,
-                size=len(param_dict["pm"]),
-            )
-        elif prior_type == "uniform":
-            pm_params = parameter.Uniform(
-                prior_lower_bound, prior_upper_bound, size=len(param_dict["pm"])
-            )
-        else:
-            raise ValueError(
-                "prior_type can only be uniform or bounded-normal, not ", prior_type
-            )
-    else:
-        pm_params = None
-
-    if len(param_dict["spin"]) != 0:
-        if prior_type == "bounded-normal":
-            spin_params = BoundedNormal(
-                mu=0.0,
-                sigma=prior_sigma,
-                pmin=prior_lower_bound,
-                pmax=prior_upper_bound,
-                size=len(param_dict["spin"]),
-            )
-        elif prior_type == "uniform":
-            spin_params = parameter.Uniform(
-                prior_lower_bound, prior_upper_bound, size=len(param_dict["spin"])
-            )
-        else:
-            raise ValueError(
-                "prior_type can only be uniform or bounded-normal, not ", prior_type
-            )
-    else:
-        spin_params = None
-    if len(param_dict["kep"]) != 0:
-        if prior_type == "bounded-normal":
-            kep_params = BoundedNormal(
-                mu=0.0,
-                sigma=prior_sigma,
-                pmin=prior_lower_bound,
-                pmax=prior_upper_bound,
-                size=len(param_dict["kep"]),
-            )
-        elif prior_type == "uniform":
-            kep_params = parameter.Uniform(
-                prior_lower_bound, prior_upper_bound, size=len(param_dict["kep"])
-            )
-        else:
-            raise ValueError(
-                "prior_type can only be uniform or bounded-normal, not ", prior_type
-            )
-    else:
-        kep_params = None
-    if len(param_dict["gr"]) != 0:
-        if prior_type == "bounded-normal":
-            gr_params = BoundedNormal(
-                mu=0.0,
-                sigma=prior_sigma,
-                pmin=prior_lower_bound,
-                pmax=prior_upper_bound,
-                size=len(param_dict["gr"]),
-            )
-        elif prior_type == "uniform":
-            gr_params = parameter.Uniform(
-                prior_lower_bound, prior_upper_bound, size=len(param_dict["gr"])
-            )
-        else:
-            raise ValueError(
-                "prior_type can only be uniform or bounded-normal, not ", prior_type
-            )
-    else:
-        gr_params = None
-
     # timing model
-    tm_func = tm_delay(
-        param_dict=param_dict,
-        pos_params=pos_params,
-        pm_params=pm_params,
-        spin_params=spin_params,
-        kep_params=kep_params,
-        gr_params=gr_params,
-    )
+    tm_func = tm_delay(**tm_delay_kwargs)
     tm = deterministic_signals.Deterministic(tm_func, name="timing_model")
 
     return tm
