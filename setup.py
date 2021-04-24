280044cdcead7cef8e2f3e086a137ef5daafd6e3
Justin Ellis <justin.ellis18@gmail.com>
2021-04-24 17:01:38 -0400

    please work

--- setup.py
+++ setup.py
@@ -35,8 +35,10 @@ test_requirements = [
 
 if platform.system() == "Darwin":
     extra_compile_args = ["-O2", "-Xpreprocessor", "-fopenmp", "-fno-wrapv"]
+    exxtra_link_args = ["-liomp5"] if os.getenv("NO_MKL", 0) == 0 else ["-lomp"]
 else:
     extra_compile_args = ["-O2", "-fopenmp", "-fno-wrapv"]
+    extra_link_args = ["-liomp5"]
 
 
 ext_modules = [
@@ -50,7 +52,7 @@ ext_modules = [
         "enterprise_extensions.outlier.choleskyext_omp",
         ["./enterprise_extensions/outlier/choleskyext_omp.pyx"],
         include_dirs=[numpy.get_include()],
-        extra_link_args=["-liomp5"] if os.getenv("NO_MKL", 0) == 0 else ["-lomp"],
+        extra_link_args=extra_link_args,
         extra_compile_args=extra_compile_args,
     ),
 ]
