5f92d05f8c571392be5d16187aaa453cb9894e1e
William G Lamb <60004701+astrolamb@users.noreply.github.com>
2021-06-24 12:47:13 -0500

    fix to empirical distribution block bugs (#117)
    * fixed empirical distribution block
    
    fixes to empirical distribution block bugs
    previous version would let you input a path to a directory of empirical distributions and make the program appear to use them, but this was false.
    It would also fail is you input a list of empirical distributions.
    
    This fix:
    - lets you input a list of empirical distributions
    - point towards a directory of empirical distrubtion pkls
    - input a single pkl file
    
    If an input for empirical distribution is given, the code won't fail, but rather it would set self.empirical_distr to its default value (None) and print a helpful message to say that the empirical distributions couldn't be read
    
    * importing glob
    
    * fixed indents
    
    * fix printed line break
    
    * added comments to the try-catch block

--- enterprise_extensions/sampler.py
+++ enterprise_extensions/sampler.py
@@ -6,6 +6,7 @@ import os
 from enterprise import constants as const
 import pickle
 import healpy as hp
+import glob
 
 from enterprise import constants as const
 from PTMCMCSampler.PTMCMCSampler import PTSampler as ptmcmc
@@ -46,23 +47,52 @@ class JumpProposal(object):
         else:
             self.snames = snames
 
-        # empirical distributions
-        if empirical_distr is not None and os.path.isfile(empirical_distr):
+        # empirical distributions 
+        if isinstance(empirical_distr, list):
+            # check if a list of emp dists is provided
+            self.empirical_distr = empirical_distr
+        
+        # check if a directory of empirical dist pkl files are provided
+        elif empirical_distr is not None and os.path.isdir(empirical_distr):
+        
+            dir_files = glob.glob(empirical_distr+'*.pkl') # search for pkls
+        
+            pickled_distr = np.array([])
+            for idx, emp_file in enumerate(dir_files):
+                try:
+                    with open(emp_file, 'rb') as f:
+                        pickled_distr = np.append(pickled_distr, pickle.load(f))
+                except:
+                    try:
+                        with open(emp_file, 'rb') as f:
+                           pickled_distr = np.append(pickled_distr, pickle.load(f))
+                    except:
+                        print(f'\nI can\'t open the empirical distribution pickle file at location {idx} in list!')
+                        print("Empirical distributions set to 'None'")
+                        pickled_distr = None
+                        break
+            
+            self.empirical_distr = pickled_distr
+        
+        # check if single pkl file provided
+        elif empirical_distr is not None and os.path.isfile(empirical_distr):  # checking for single file
             try:
+                # try opening the file
                 with open(empirical_distr, 'rb') as f:
                     pickled_distr = pickle.load(f)
             except:
+                # second attempt at opening the file
                 try:
                     with open(empirical_distr, 'rb') as f:
                         pickled_distr = pickle.load(f)
+                # if the second attempt fails...
                 except:
-                    print('I can\'t open the empirical distribution pickle file!')
+                    print('\nI can\'t open the empirical distribution pickle file!')
                     pickled_distr = None
 
             self.empirical_distr = pickled_distr
-
-        elif isinstance(empirical_distr,list):
-            pass
+        
+        # all other cases - emp dists set to None
         else:
             self.empirical_distr = None
 
@@ -80,6 +110,10 @@ class JumpProposal(object):
                 self.empirical_distr = [self.empirical_distr[m] for m in mask]
             else:
                 self.empirical_distr = None
+                
+        if empirical_distr is not None and self.empirical_distr is None:
+            # if an emp dist path is provided, but fails the code, this helpful msg is provided
+            print("Adding empirical distributions failed!! Empirical distributions set to 'None'\n")
 
         #F-statistic map
         if f_stat_file is not None and os.path.isfile(f_stat_file):
@@ -790,7 +824,7 @@ def setup_sampler(pta, outdir='chains', resume=False, empirical_distr=None):
 
     # try adding empirical proposals
     if empirical_distr is not None:
-        print('Adding empirical proposals...\n')
+        print('Attempting to add empirical proposals...\n')
         sampler.addProposalToCycle(jp.draw_from_empirical_distr, 10)
 
     # Red noise prior draw
