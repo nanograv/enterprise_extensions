4f458c16b90957b209815333e9e6eadf78f7b251
Aaron <Aaron9034@Hotmail.com>
2021-09-01 01:32:11 -0500

    Timing model comparison dense cholesky

--- enterprise_extensions/model_utils.py
+++ enterprise_extensions/model_utils.py
@@ -302,35 +302,53 @@ class CompareTimingModels():
 
     :param psrs: Pulsar object containing pulsars from model
     :param model_name: String name of model to test. Model must be defined in enterprise_extensions.models.
-    :param abs_tol: absolute tolerance for error between timing models (default 1e-3)
-    :param rel_tol: relative tolerance for error between timing models (default 1e-6)
+    :param abs_tol: absolute tolerance for error between timing models (default 1e-3), set to None to bypass errors
+    :param rel_tol: relative tolerance for error between timing models (default 1e-6), set to None to bypass errors
+    :param dense: use the dense cholesky algorithm over sparse
     """
-    def __init__(self, psrs, model_name='model_1', abs_tol=1e-3, rel_tol=1e-6, **kwargs):
+    def __init__(self, psrs, model_name='model_1', abs_tol=1e-3, rel_tol=1e-6, dense=True, **kwargs):
         model = getattr(models, model_name)
         self.abs_tol = abs_tol
         self.rel_tol = rel_tol
-        self.pta_marg = model(psrs, tm_marg=True, **kwargs)  # marginalized model
+        if dense:
+            self.pta_marg = model(psrs, tm_marg=True, dense_like=True, **kwargs)  # marginalized model
+        else:
+            self.pta_marg = model(psrs, tm_marg=True, **kwargs)  # marginalized model
         self.pta_norm = model(psrs, **kwargs)  # normal model
-        # self.like_correction = 0
-        # for psr in psrs:
-        #     self.like_correction -= 0.5 * np.log(1e40) * psr.Mmat.shape[1]
+        self.tm_correction = 0
+        for psr in psrs:
+            self.tm_correction -= 0.5 * np.log(1e40) * psr.Mmat.shape[1]
         self.abs_err = []
         self.rel_err = []
         self.count = 0
 
-    def check_timing(self, number=10000):
-        x0 = np.hstack([p.sample() for p in self.pta_marg.params])
+    def check_timing(self, number=10_000):
+        print('Timing sample creation...')
         start = time.time()
         for __ in range(number):
+            x0 = np.hstack([p.sample() for p in self.pta_marg.params])
+        end = time.time()
+        sample_time = end - start
+        print('Sampling {0} points took {1} seconds.'.format(number, sample_time))
+
+        print('Timing MarginalizedTimingModel...')
+        start = time.time()
+        for __ in range(number):
+            x0 = np.hstack([p.sample() for p in self.pta_marg.params])
             self.pta_marg.get_lnlikelihood(x0)
         end = time.time()
-        time_marg = end - start
+        time_marg = end - start - sample_time  # remove sampling time from total time taken
+        print('Sampling {0} points took {1} seconds.'.format(number, time_marg))
 
+        print('Timing TimingModel...')
         start = time.time()
         for __ in range(number):
+            x0 = np.hstack([p.sample() for p in self.pta_marg.params])
             self.pta_norm.get_lnlikelihood(x0)
         end = time.time()
-        time_norm = end - start
+        time_norm = end - start - sample_time  # remove sampling time from total time taken
+        print('Sampling {0} points took {1} seconds.'.format(number, time_norm))
+
         res = time_norm / time_marg
         print('MarginalizedTimingModel is {0} times faster than TimingModel after {1} points.'.format(res, number))
         return res
@@ -347,10 +365,10 @@ class CompareTimingModels():
         self.abs_err.append(abs_err)
         self.rel_err.append(rel_err)
         self.count += 1
-        if abs_err > self.abs_tol:
+        if self.abs_tol is not None and abs_err > self.abs_tol:
             abs_raise = 'Absolute error is {0} at {1} which is larger than abs_tol of {2}.'.format(abs_err, x0, self.abs_tol)
             raise ValueError(abs_raise)
-        elif rel_err > self.rel_tol:
+        elif self.rel_tol is not None and rel_err > self.rel_tol:
             rel_raise = 'Relative error is {0} at {1} which is larger than rel_tol of {2}.'.format(rel_err, x0, self.rel_tol)
             raise ValueError(rel_raise)
         return res_norm
@@ -359,4 +377,4 @@ class CompareTimingModels():
         print('Number of points evaluated:', self.count)
         print('Maximum absolute error:', np.max(self.abs_err))
         print('Maximum relative error:', np.max(self.rel_err))
-        return np.max(self.abs_err), np.max(self.rel_err)
+        return self.abs_err, self.rel_err
