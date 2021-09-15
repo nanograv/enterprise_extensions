e3119073fe10ee413b22f0900ee99c2a374431ac
Hazboun6 <jeffrey.hazboun@gmail.com>
2021-09-15 09:53:39 -0700

    cleanup old compile commands. rm test reqs

--- setup.py
+++ setup.py
@@ -26,16 +26,7 @@ requirements = [
     "ptmcmcsampler",
 ]
 
-test_requirements = [
-    "pytest",
-]
-
-if platform.system() == "Darwin":
-    extra_compile_args = ["-O2", "-Xpreprocessor", "-fopenmp", "-fno-wrapv"]
-    extra_link_args = ["-liomp5"] if os.getenv("NO_MKL", 0) == 0 else ["-lomp"]
-else:
-    extra_compile_args = ["-O2", "-fopenmp", "-fno-wrapv"]
-    extra_link_args = ["-liomp5"] if os.getenv("NO_MKL", 0) == 0 else []
+test_requirements = []
 
 # Extract version
 def get_version():
