3b177554552765261157f7171b979df8e2890438
Sophie Hourihane <shouriha@umich.edu>
2022-10-19 11:58:25 -0700

    Adding signal_model options to data_class (BETA!)

--- enterprise_extensions/pipe/data_class.py
+++ enterprise_extensions/pipe/data_class.py
@@ -1,12 +1,13 @@
 from dataclasses import asdict, dataclass, field
 import configparser
 import inspect
-import enterprise.signals.parameter # this is used but only implicitly
+import enterprise.signals.parameter  # this is used but only implicitly
 import enterprise_extensions.models
 import collections.abc
 import pickle, json
 import importlib
 import numpy as np
+from enterprise.signals import signal_base
 
 
 def get_default_args_types_from_function(func):
@@ -43,16 +44,23 @@ def update_dictionary_with_subdictionary(d, u):
 
 @dataclass
 class RunSettings:
-    """Class for keeping track of enterprise model run settings"""
+    """
+    Class for keeping track of enterprise model run settings
+    TODO: link to examples of how to use
+    """
     config_file: str = None
     pulsar_pickle: str = None
     noise_dict_json: str = None
 
+    # dictionary of functions that create signals
+    signal_creating_functions = {}
+    signal_creating_function_parameters = {}
+    # dictionary of functions that create pta objects
     pta_creating_function_parameters = {}
     pta_creating_functions = {}
 
     custom_classes = {}
-    custom_function_returns = {}
+    custom_function_return = {}
 
     psrs = None
     noise_dict = None
@@ -61,9 +69,11 @@ class RunSettings:
         """
         Set defaults for functions from file
         """
-        config = configparser.ConfigParser(comment_prefixes=';', interpolation=configparser.ExtendedInterpolation())
+        config = configparser.ConfigParser(comment_prefixes=';',
+                                           interpolation=configparser.ExtendedInterpolation())
         config.optionxform = str
         config.read(config_file)
+        exclude_keys = ['function', 'module', 'class', 'signal_return', 'pta_return', 'custom_return']
         for section in config.sections():
             config_file_items = dict(config.items(section))
 
@@ -76,7 +86,7 @@ class RunSettings:
 
             elif 'class' in config_file_items.keys():
                 """
-                Get default values for a module defined elsewhere
+                Initialize a class given in a config file 
                 """
                 # Import a module defined elsewhere
                 module = importlib.import_module(config_file_items['module'])
@@ -86,9 +96,10 @@ class RunSettings:
 
                 class_parameters, types = get_default_args_types_from_function(custom_class.__init__)
                 class_parameters_from_file = self.apply_types(config_file_items, types,
-                                                         exclude_keys=['module', 'class'])
+                                                              exclude_keys=exclude_keys)
                 class_parameters = update_dictionary_with_subdictionary(class_parameters, class_parameters_from_file)
                 self.custom_classes[section] = custom_class(**class_parameters)
+
             elif 'function' in config_file_items.keys():
                 # import a module defined elsewhere
                 module = importlib.import_module(config_file_items['module'])
@@ -96,28 +107,41 @@ class RunSettings:
                 custom_function = getattr(module, config_file_items['function'])
                 function_parameters, types = get_default_args_types_from_function(custom_function)
                 function_parameters_from_file = self.apply_types(config_file_items, types,
-                                                            exclude_keys=['module', 'function'])
+                                                                 exclude_keys=exclude_keys)
 
-                if 'returns' in config_file_items.keys():
-                    self.custom_function_returns[config_file_items['returns']] = custom_function(function_parameters_from_file)
-                else:
-                    # TODO not sure what to do if function is not PTA creating...
+                if 'custom_return' in config_file_items.keys():
+                    # custom_return means that this function is just being called to return something else
+                    self.custom_function_return[config_file_items['custom_return']] = \
+                        custom_function(**function_parameters_from_file)
+                    continue
+                elif 'signal_return' in config_file_items.keys():
+                    self.signal_creating_functions[section] = custom_function
+                    self.signal_creating_function_parameters[section] = update_dictionary_with_subdictionary(
+                        function_parameters,
+                        function_parameters_from_file)
+                elif 'pta_return' in config_file_items.keys():
                     self.pta_creating_functions[section] = custom_function
                     self.pta_creating_function_parameters[section] = update_dictionary_with_subdictionary(
-                                                                                function_parameters,
-                                                                               function_parameters_from_file)
+                        function_parameters,
+                        function_parameters_from_file)
+                else:
+                    raise (AttributeError((
+                        "'function' needs one of 'custom_return=SOMETHING' "
+                        "'signal_function=True' 'pta_function=True' "
+                        "in .ini file")))
             else:
                 try:
                     """
                     Get default values for models held in enterprise_extensions
                     """
                     model_function = getattr(enterprise_extensions.models, section)
-                    self.pta_creating_function_parameters[section], types = get_default_args_types_from_function(model_function)
+                    self.pta_creating_function_parameters[section], types = get_default_args_types_from_function(
+                        model_function)
                     # Update default args with those held inside of path
-                    config_file_items = {k: types[k](config_file_items[k]) for k in config_file_items}
+                    function_parameters_from_file = self.apply_types(config_file_items, types)
                     self.pta_creating_function_parameters[section] = \
                         update_dictionary_with_subdictionary(self.pta_creating_function_parameters[section],
-                                                             config_file_items)
+                                                             function_parameters_from_file)
                     self.pta_creating_functions[section] = model_function
                 except AttributeError as e:
                     # TODO this should probably exit
@@ -127,15 +151,23 @@ class RunSettings:
 
     def apply_types(self, dictionary, type_dictionary, exclude_keys=[]):
         """
-        Given dictionary (from config_file) and dictionary containing types
+        Given dictionary (usually created from config_file) and dictionary containing types
         apply type to dictionary
 
-        Note: if CUSTOM_CLASS:your_class is in dictionary[key],
-        then instead of applying type,
+        if CUSTOM_CLASS:your_class is in dictionary[key],
+            instead of applying type it assigns from self.custom_classes
+        if CUSTOM_RETURN:whatever is in dictionary[key]
+            instead of applying type it assigns from self.custom_returns[whatever]
+        if FUNCTION_CALL:whatever
+            will call eval("whatever") and assign that
         """
         out_dictionary = {}
         for key, value in dictionary.items():
             if key in exclude_keys:
+                continue
+            if 'CUSTOM_FUNCTION_RETURN:' in value:
+                # Apply custom class instance stored in custom_classes
+                out_dictionary[key] = self.custom_function_return[value.replace('CUSTOM_FUNCTION_RETURN:', '')]
                 continue
             if 'CUSTOM_CLASS:' in value:
                 # Apply custom class instance stored in custom_classes
@@ -160,8 +192,6 @@ class RunSettings:
                 out_dictionary[key] = type_dictionary[key](value)
 
         return out_dictionary
-
-
 
     def update_from_dict(self, **kwargs):
         ann = getattr(self, "__annotations__", {})
@@ -196,23 +226,44 @@ class RunSettings:
                 self.pta_creating_function_parameters[key]['noisedict'] = self.noise_dict
 
     def create_pta_object_from_signals(self):
-        raise NotImplementedError
+        """
+        Using both signals from pta objects and signals from self.signal_creating_functions
+        Create a pta object
+        """
+        pta_list = self.get_pta_objects()
+        signal_collections = [self.get_signal_collection_from_pta_object(pta) for pta in pta_list]
+        for key, func in self.signal_creating_functions.items():
+            signal_collections.append(func(**self.signal_creating_function_parameters[key]))
 
-    def create_pta_object(self):
+        signal_collection = sum(signal_collections[1:], signal_collections[0])
+
+        model_list = [signal_collection(psr) for psr in self.psrs]
+        pta = signal_base.PTA(model_list)
+
+        # apply noise dictionary to pta
+        pta.set_default_params(self.noise_dict)
+        # return pta object
+        return pta
+
+    def get_signal_collection_from_pta_object(self, pta):
         """
-        Using enterprise_models, create a PTA object
+        Under assumption that same model has been applied to ALL pulsars
+        and that there are pulsars inside of this pta,
+        get signal collection from this pta object
         """
+        return type(pta.pulsarmodels[0])
+
+
+
+    def get_pta_objects(self):
+        """
+        Using pta creating functions specified in config, get list of pta objects
+        """
+        pta_list = []
         if self.psrs is None:
             self.load_pickled_pulsars()
 
-        keys = list(self.pta_creating_function_parameters.keys())
-        if len(keys) == 0:
-            print("WARNING: create_pta_object, there are no pta_creating_functions!")
-            raise IndexError
-
-        pta = self.pta_creating_functions[keys[0]](psrs=self.psrs, **self.pta_creating_function_parameters[keys[0]])
-        for key in keys[1:]:
-            pta = pta + self.pta_creating_functions[key](psrs=self.psrs,
-                                                             **self.pta_creating_function_parameters[key])
-
-        return pta
+        for key in self.pta_creating_function_parameters.keys():
+            pta_list.append(self.pta_creating_functions[key](psrs=self.psrs,
+                                                             **self.pta_creating_function_parameters[key]))
+        return pta_list
