d6676318852b84889dc1383c3026b9e668d66dd1
Rutger van Haasteren <vhaasteren@gmail.com>
2024-04-15 09:38:59 +0200

    Linting

--- enterprise_extensions/frequentist/Fe_statistic.py
+++ enterprise_extensions/frequentist/Fe_statistic.py
@@ -14,15 +14,16 @@ Mar 2024, Kathrin Grunthal (EPTA):  assure backward-compatibility
 
 import numpy as np
 import scipy.linalg as sl
-from enterprise.signals import (gp_signals, parameter, selections, signal_base, utils,
-                                white_signals)
+from enterprise.signals import (
+    gp_signals,
+    parameter,
+    selections,
+    signal_base,
+    utils,
+    white_signals,
+)
 from enterprise_extensions import blocks
 from . import cgw_model
-
-
-
-
-
 
 
 class FeStat(object):
@@ -34,11 +35,12 @@ class FeStat(object):
 
     """
 
-    def __init__(self, psrs, params=None, custom_models={}, inc_crn=False, orf=None, pta=None):
-
+    def __init__(
+        self, psrs, params=None, custom_models={}, inc_crn=False, orf=None, pta=None
+    ):
         if pta is None:
-            print('Creating a PTA with TM and WN')
-            
+            print("Creating a PTA with TM and WN")
+
             efac = parameter.Constant()
             equad = parameter.Constant()
             ef = white_signals.MeasurementNoise(efac=efac)
@@ -55,7 +57,7 @@ class FeStat(object):
 
             # set white noise parameters
             if params is None:
-                print('No noise dictionary provided!')
+                print("No noise dictionary provided!")
             else:
                 self.pta.set_default_params(params)
 
@@ -66,10 +68,16 @@ class FeStat(object):
         self.params = params
         self.Nvecs = None
 
-        
-
-    #--------------------------------------------------------------------------
-    def compute_Fe(self, f0, gw_skyloc, brave=False, maximized_parameters=False, sky_scrambles=False, psrs_theta_phi=None):
+    # --------------------------------------------------------------------------
+    def compute_Fe(
+        self,
+        f0,
+        gw_skyloc,
+        brave=False,
+        maximized_parameters=False,
+        sky_scrambles=False,
+        psrs_theta_phi=None,
+    ):
         """
         Computes the Fe-statistic (see Ellis, Siemens, Creighton 2012).
 
@@ -90,7 +98,7 @@ class FeStat(object):
 
         """
 
-        tref=53000*86400
+        tref = 53000 * 86400
 
         phiinvs = self.pta.get_phiinv(self.params, logdet=False)
         TNTs = self.pta.get_TNT(self.params)
@@ -99,14 +107,13 @@ class FeStat(object):
         if self.Nvecs is None:
             self.Nvecs = self.pta.get_ndiag(self.params)
 
-
         n_psr = len(self.psrs)
         N = np.zeros((n_psr, 4))
         M = np.zeros((n_psr, 4, 4))
 
-        for idx, (psr, Nvec, TNT, phiinv, T) in enumerate(zip(self.psrs, self.Nvecs,
-                                                              TNTs, phiinvs, Ts)):
-            
+        for idx, (psr, Nvec, TNT, phiinv, T) in enumerate(
+            zip(self.psrs, self.Nvecs, TNTs, phiinvs, Ts)
+        ):
             # calculate non-changing terms for the inner product
             # to avoid redundant calculations in innerProduct_rr
             Sigma = TNT + (np.diag(phiinv) if phiinv.ndim == 1 else phiinv)
@@ -114,22 +121,32 @@ class FeStat(object):
             ntoa = len(psr.toas)
 
             A = np.zeros((4, ntoa))
-            A[0, :] = 1 / f0 ** (1 / 3) * np.sin(2 * np.pi * f0 * (psr.toas-tref))
-            A[1, :] = 1 / f0 ** (1 / 3) * np.cos(2 * np.pi * f0 * (psr.toas-tref))
-            A[2, :] = 1 / f0 ** (1 / 3) * np.sin(2 * np.pi * f0 * (psr.toas-tref))
-            A[3, :] = 1 / f0 ** (1 / 3) * np.cos(2 * np.pi * f0 * (psr.toas-tref))
+            A[0, :] = 1 / f0 ** (1 / 3) * np.sin(2 * np.pi * f0 * (psr.toas - tref))
+            A[1, :] = 1 / f0 ** (1 / 3) * np.cos(2 * np.pi * f0 * (psr.toas - tref))
+            A[2, :] = 1 / f0 ** (1 / 3) * np.sin(2 * np.pi * f0 * (psr.toas - tref))
+            A[3, :] = 1 / f0 ** (1 / 3) * np.cos(2 * np.pi * f0 * (psr.toas - tref))
 
-            ip1 = innerProduct_rr(A[0, :], psr.residuals, Nvec, T, TNT, Sigma, brave=brave)
-            ip2 = innerProduct_rr(A[1, :], psr.residuals, Nvec, T, TNT, Sigma, brave=brave)
-            ip3 = innerProduct_rr(A[2, :], psr.residuals, Nvec, T, TNT, Sigma, brave=brave)
-            ip4 = innerProduct_rr(A[3, :], psr.residuals, Nvec, T, TNT, Sigma, brave=brave)
-        
+            ip1 = innerProduct_rr(
+                A[0, :], psr.residuals, Nvec, T, TNT, Sigma, brave=brave
+            )
+            ip2 = innerProduct_rr(
+                A[1, :], psr.residuals, Nvec, T, TNT, Sigma, brave=brave
+            )
+            ip3 = innerProduct_rr(
+                A[2, :], psr.residuals, Nvec, T, TNT, Sigma, brave=brave
+            )
+            ip4 = innerProduct_rr(
+                A[3, :], psr.residuals, Nvec, T, TNT, Sigma, brave=brave
+            )
+
             N[idx, :] = np.array([ip1, ip2, ip3, ip4])
 
             # define M matrix M_ij=(A_i|A_j)
             for jj in range(4):
                 for kk in range(4):
-                    M[idx, jj, kk] = innerProduct_rr(A[jj, :], A[kk, :], Nvec, T, TNT, Sigma, brave=brave)
+                    M[idx, jj, kk] = innerProduct_rr(
+                        A[jj, :], A[kk, :], Nvec, T, TNT, Sigma, brave=brave
+                    )
 
         fstat = np.zeros(gw_skyloc.shape[1])
         if maximized_parameters:
@@ -144,20 +161,34 @@ class FeStat(object):
             for idx, psr in enumerate(self.psrs):
                 if sky_scrambles:
                     if psrs_theta_phi is None:
-                        ptheta = np.arccos(np.random.uniform(-1., 1.))
-                        pphi = np.random.uniform(0., 2*np.pi)
+                        ptheta = np.arccos(np.random.uniform(-1.0, 1.0))
+                        pphi = np.random.uniform(0.0, 2 * np.pi)
                     else:
                         ptheta = psrs_theta_phi[idx, 0]
                         pphi = psrs_theta_phi[idx, 1]
-                    pos = np.array([np.cos(pphi)*np.sin(ptheta), np.sin(pphi)*np.sin(ptheta), np.cos(ptheta)])
-                    F_p, F_c, _ = utils.create_gw_antenna_pattern(pos, gw_pos[0], gw_pos[1])
+                    pos = np.array(
+                        [
+                            np.cos(pphi) * np.sin(ptheta),
+                            np.sin(pphi) * np.sin(ptheta),
+                            np.cos(ptheta),
+                        ]
+                    )
+                    F_p, F_c, _ = utils.create_gw_antenna_pattern(
+                        pos, gw_pos[0], gw_pos[1]
+                    )
                 else:
-                    F_p, F_c, _ = utils.create_gw_antenna_pattern(psr.pos, gw_pos[0], gw_pos[1])
+                    F_p, F_c, _ = utils.create_gw_antenna_pattern(
+                        psr.pos, gw_pos[0], gw_pos[1]
+                    )
                 NN[idx, :] *= np.array([F_p, F_p, F_c, F_c])
-                MM[idx, :, :] *= np.array([[F_p**2, F_p**2, F_p*F_c, F_p*F_c],
-                                           [F_p**2, F_p**2, F_p*F_c, F_p*F_c],
-                                           [F_p*F_c, F_p*F_c, F_c**2, F_c**2],
-                                           [F_p*F_c, F_p*F_c, F_c**2, F_c**2]])
+                MM[idx, :, :] *= np.array(
+                    [
+                        [F_p**2, F_p**2, F_p * F_c, F_p * F_c],
+                        [F_p**2, F_p**2, F_p * F_c, F_p * F_c],
+                        [F_p * F_c, F_p * F_c, F_c**2, F_c**2],
+                        [F_p * F_c, F_p * F_c, F_c**2, F_c**2],
+                    ]
+                )
 
             N_sum = np.sum(NN, axis=0)
             M_sum = np.sum(MM, axis=0)
@@ -170,80 +201,79 @@ class FeStat(object):
             if maximized_parameters:
                 a_hat = np.dot(Minv, N_sum)
 
-                A_p = (np.sqrt((a_hat[0]+a_hat[3])**2 + (a_hat[1]-a_hat[2])**2) +
-                       np.sqrt((a_hat[0]-a_hat[3])**2 + (a_hat[1]+a_hat[2])**2))
-                A_c = (np.sqrt((a_hat[0]+a_hat[3])**2 + (a_hat[1]-a_hat[2])**2) -
-                       np.sqrt((a_hat[0]-a_hat[3])**2 + (a_hat[1]+a_hat[2])**2))
+                A_p = np.sqrt(
+                    (a_hat[0] + a_hat[3]) ** 2 + (a_hat[1] - a_hat[2]) ** 2
+                ) + np.sqrt((a_hat[0] - a_hat[3]) ** 2 + (a_hat[1] + a_hat[2]) ** 2)
+                A_c = np.sqrt(
+                    (a_hat[0] + a_hat[3]) ** 2 + (a_hat[1] - a_hat[2]) ** 2
+                ) - np.sqrt((a_hat[0] - a_hat[3]) ** 2 + (a_hat[1] + a_hat[2]) ** 2)
                 AA = A_p + np.sqrt(A_p**2 - A_c**2)
                 # AA = A_p + np.sqrt(A_p**2 + A_c**2)
 
                 # inc_max[j] = np.arccos(-A_c/AA)
-                inc_max[j] = np.arccos(A_c/AA)
+                inc_max[j] = np.arccos(A_c / AA)
 
-                two_psi_max = np.arctan2((A_p*a_hat[3] - A_c*a_hat[0]),
-                                         (A_c*a_hat[2] + A_p*a_hat[1]))
+                two_psi_max = np.arctan2(
+                    (A_p * a_hat[3] - A_c * a_hat[0]), (A_c * a_hat[2] + A_p * a_hat[1])
+                )
 
-                psi_max[j]=0.5*np.arctan2(np.sin(two_psi_max),
-                                          -np.cos(two_psi_max))
+                psi_max[j] = 0.5 * np.arctan2(np.sin(two_psi_max), -np.cos(two_psi_max))
 
                 # convert from [-pi, pi] convention to [0,2*pi] convention
-                if psi_max[j]<0:
-                    psi_max[j]+=np.pi
+                if psi_max[j] < 0:
+                    psi_max[j] += np.pi
 
                 # correcting weird problem of degeneracy (psi-->pi-psi/2 and phi0-->2pi-phi0 keep everything the same)
-                if psi_max[j]>np.pi/2:
-                    psi_max[j]+= -np.pi/2
+                if psi_max[j] > np.pi / 2:
+                    psi_max[j] += -np.pi / 2
 
-                half_phase0 = -0.5*np.arctan2(A_p*a_hat[3] - A_c*a_hat[0],
-                                              A_c*a_hat[1] + A_p*a_hat[2])
+                half_phase0 = -0.5 * np.arctan2(
+                    A_p * a_hat[3] - A_c * a_hat[0], A_c * a_hat[1] + A_p * a_hat[2]
+                )
 
-                phase0_max[j] = np.arctan2(-np.sin(2*half_phase0),
-                                           np.cos(2*half_phase0))
+                phase0_max[j] = np.arctan2(
+                    -np.sin(2 * half_phase0), np.cos(2 * half_phase0)
+                )
 
                 # convert from [-pi, pi] convention to [0,2*pi] convention
-                if phase0_max[j]<0:
-                    phase0_max[j]+=2*np.pi
+                if phase0_max[j] < 0:
+                    phase0_max[j] += 2 * np.pi
 
-                zeta = np.abs(AA)/4  # related to amplitude, zeta=M_chirp^(5/3)/D
-                h_max[j] = zeta * 2 * (np.pi*f0)**(2/3)*np.pi**(1/3)
+                zeta = np.abs(AA) / 4  # related to amplitude, zeta=M_chirp^(5/3)/D
+                h_max[j] = zeta * 2 * (np.pi * f0) ** (2 / 3) * np.pi ** (1 / 3)
 
         if maximized_parameters:
             return fstat, inc_max, psi_max, phase0_max, h_max
         else:
             return fstat
-        
 
 
-
-#==============================================================================
+# ==============================================================================
 def innerProduct_rr(x, y, Nvec, Tmat, TNT, Sigma, brave=False):
     r"""
-        Compute inner product using rank-reduced
-        approximations for red noise/jitter
-        Compute: x^T N^{-1} y - x^T N^{-1} T \Sigma^{-1} T^T N^{-1} y
+    Compute inner product using rank-reduced
+    approximations for red noise/jitter
+    Compute: x^T N^{-1} y - x^T N^{-1} T \Sigma^{-1} T^T N^{-1} y
 
-        :param x: vector timeseries 1
-        :param y: vector timeseries 2
-        :param Nvec: white noise Sherman Morrison object
-        :param Tmat: Modified design matrix including red noise/jitter
-        :param TNT: T^T N^{-1} T matrix from pta object
-        :param Sigma: Sigma matrix (\varphi^{-1} + T^T N^{-1} T)
+    :param x: vector timeseries 1
+    :param y: vector timeseries 2
+    :param Nvec: white noise Sherman Morrison object
+    :param Tmat: Modified design matrix including red noise/jitter
+    :param TNT: T^T N^{-1} T matrix from pta object
+    :param Sigma: Sigma matrix (\varphi^{-1} + T^T N^{-1} T)
 
-        :return: inner product (x|y)
-        """
+    :return: inner product (x|y)
+    """
 
-    TNy = Nvec.solve(y, left_array = Tmat)
-    TNx = Nvec.solve(x, left_array = Tmat)  # later used only in transposed version
-    xNy = Nvec.solve(y, left_array = x)
-    
+    TNy = Nvec.solve(y, left_array=Tmat)
+    TNx = Nvec.solve(x, left_array=Tmat)  # later used only in transposed version
+    xNy = Nvec.solve(y, left_array=x)
+
     if brave:
         cf = sl.cho_factor(Sigma, check_finite=False)
         SigmaTNy = sl.cho_solve(cf, TNy, check_finite=False)
     else:
         cf = sl.cho_factor(Sigma)
         SigmaTNy = sl.cho_solve(cf, TNy)
-        
-    
+
     return xNy - TNx.T @ SigmaTNy
-
-
