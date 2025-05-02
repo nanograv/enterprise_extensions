985b9616f7e119403758f3a400828d40e4924036
Rutger van Haasteren <vhaasteren@gmail.com>
2025-05-02 15:54:28 +0200

    First p-value enabled DetectionStatistic working

--- enterprise_extensions/frequentist/optimal_statistic.py
+++ enterprise_extensions/frequentist/optimal_statistic.py
@@ -4,6 +4,7 @@ import warnings
 
 import numpy as np
 import scipy.linalg as sl
+import scipy.integrate as sint
 from enterprise.signals import gp_priors, signal_base, utils
 
 from enterprise_extensions import model_orfs, models
@@ -16,6 +17,33 @@ def warning_on_one_line(message, category, filename, lineno, file=None, line=Non
 
 # Override default format.
 warnings.formatwarning = warning_on_one_line
+
+
+def imhof(u, x, eigen_values, output='cdf'):
+    theta = 0.5 * np.sum(np.arctan(eigen_values[:,np.newaxis] * u), axis=0) - 0.5 * x * u
+    rho = np.prod((1.0 + (eigen_values[:,np.newaxis] * u)**2)**0.25, axis=0)
+
+    rv = np.sin(theta) / (u * rho) if output=='cdf' else np.cos(theta) / rho
+
+    return rv
+
+
+def gx2pdf(eigen_values, xs, cutoff=1e-6, limit=100, epsabs=1e-6):
+    """Calculate the GX2 PDF as a function of sx, based off of eigenvalues 'eigen_values'"""
+
+    eigen_values = eigen_values[:cutoff] if cutoff > 1 else eigen_values[np.abs(eigen_values) > cutoff]
+
+    return np.array([sint.quad(lambda u: float(imhof(u, x, eigen_values, output='pdf')),
+                                                0, np.inf, limit=limit, epsabs=epsabs)[0] / (2*np.pi) for x in xs])
+
+
+def gx2cdf(eigr, xs, cutoff=1e-6, limit=100, epsabs=1e-6):
+    """Calculate the GX2 CDF as a function of sx, based off of eigenvalues 'eigr'"""
+
+    eigen_values = eigr[:cutoff] if cutoff > 1 else eigr[np.abs(eigr) > cutoff]
+
+    return np.array([0.5 - sint.quad(lambda u: float(imhof(u, x, eigen_values)),
+                                                0, np.inf, limit=limit, epsabs=epsabs)[0] / np.pi for x in xs])
 
 
 class OptimalStatistic(object):
@@ -476,3 +504,193 @@ class OptimalStatistic(object):
             T = sc.get_basis(params=params)
             FNTs.append(N.solve(T, left_array=F))
         return FNTs
+
+
+def inv_RPR(phi, r):
+    """Invert the RphiRT matrix"""
+    I = np.identity(len(r))
+    cf = sl.cho_factor(phi)
+    phi_inv = sl.cho_solve(cf, I)
+    Sigma = r.T @ r + phi_inv
+    cf = sl.cho_factor(Sigma)
+    return I - r @ sl.cho_solve(cf, r.T)
+
+
+class DetectionStatistic(object):
+    """
+    Class for the Detection Statistic as used in the p-value paper.
+
+    This class is specifically made for classical hypothesis testing. It
+    requires an enterprise object for H0 and for H1. For now we assume that we
+    keep the white noise fixed and that the only difference between the two
+    hypotheses is in the way we model the Phi prior matrix.
+
+    :param pta_h0: The enterprise PTA object for the null hypothesis.
+    :param pta_h1: The enterprise PTA object for the signal hypothesis.
+    """
+
+    def __init__(
+        self,
+        pta_h0,
+        pta_h1
+    ):
+        # set up cache
+        self._set_cache_parameters(pta_h0, pta_h1)
+
+    def _set_cache_parameters(self, pta_h0, pta_h1):
+        """Set the cache parameters according to the Equations in van Haasteren (2025)"""
+        # N = L_N L_N^T  ==> L_N^{-T} L_N^{-1} = N^{-1}
+        # T^{prime} = L_N^{-1} Tmat         ( Tprime = NT in code )
+        # P_T T^{prime} = T^{prime}   ===> P_T = G_T G_T^T    (G_T = NU in code?)
+        # R = G_T @ Tprime = G_T @ L_N^{-1} Tmat
+        # L_0 = L_B^{-1} in code with   L_B @ L_B^T = C_i = I + R phi R^T
+        # A = L_B^{-1} G^T_T T^{prime}
+        # P_F = G_F @ G_F^T  (Project only on the basis of the common red noise)
+        # A P_F = P_A A P_F = U_A U_A^T A P_F (do with the SVD of (A P_F))
+        # Then, the final data and Q are:
+        # chi = U_A^T @ L_0 @ G_T @ L_N^{-1} @ Tmat
+        # Q = U_A^T @ L_0^T @ G_T^T @ L_N^{-1} @ F @ DeltaPhi @ F^T L_N^{-T} @ G_T @ L_0 @ U_A
+        self.pta_h0 = pta_h0
+        self.pta_h1 = pta_h1
+
+        # Calculate lists of H0 quantities (11 seconds, only need it once)
+        Tmat = pta_h0.get_basis({})           # List of 2D matrices
+        Ndiag = pta_h0.get_ndiag({})          # Objects for sqrtsolve
+        NT = [nd.sqrtsolve(tm) for (nd, tm) in zip(Ndiag, Tmat)]        # List of 2D matrices
+        G_T = [sl.svd(nt, full_matrices=False)[0] for nt in NT]        # List of 2D matrices
+        self.R = [gt.T @ nt for (gt, nt) in zip(G_T, NT)]      # List of 2D matrices
+
+        # Transformation 1:
+        Nres = [nd.sqrtsolve(r) for (nd, r) in zip(Ndiag, pta_h0.get_residuals())]   # List of 1D arrays (the weighted data)
+
+        # Transformation 2:
+        self.GTNr = [gt.T @ nr for (gt, nr) in zip(G_T, Nres)]                            # List of 1D arrays (transformed data)
+
+        # We do this here so we can avoid calculating the mask later
+        xs = np.array([par_val for p in pta_h0.params for par_val in np.atleast_1d(p.sample())])          # 1D array of all parameters
+        pd = pta_h1.map_params(xs)
+        Phi_0 = [np.diag(p) for p in pta_h0.get_phi(pd)]          # Phi matrix of H0 -- 2D arrays
+        BigPhiDiff = pta_h1.get_phi(pd) - sl.block_diag(*Phi_0)
+
+        # Get only the non-zero elements of the BigPhiDiff matrix for selections later
+        self.par_msk = (np.sum(np.abs(BigPhiDiff), axis=1)>0)                # Mask for BigPhiDiff
+        par_inds_offset = np.cumsum([0] + [len(p) for p in Phi_0])
+        par_inds_start = par_inds_offset[:-1]
+        par_inds_end = par_inds_offset[1:]
+        par_inds_slices = [np.arange(p_start, p_end) for (p_start, p_end) in zip(par_inds_start, par_inds_end)]
+        self.par_psr_msk = [self.par_msk[slc] for slc in par_inds_slices]         # Mask per psr for Phi_0 and Tmat
+
+    def _get_compressed_coordinates(self, params):
+        """Returns OS, chi, and Q for the given parameters"""
+
+        # These quantities have to be re-calculated for new hyperparameters
+        Phi_0 = [np.diag(p) for p in self.pta_h0.get_phi(params)]          # Phi matrix of H0 -- 2D arrays
+
+        # This is a BIG matrix, but it's sparse. Not using that right now though
+        # It's currently 0.4 secdonds for NG15
+        BigPhiDiff = self.pta_h1.get_phi(params) - sl.block_diag(*Phi_0)                   # 2D prior diff array
+
+        # Inverse Noise matrix
+        C2i_0 = [inv_RPR(p, r) for (r, p) in zip(self.R, Phi_0)]                  # List of matrix inverses -- 2D arrays
+
+        # Get the Square-Root (we take it from the inv for numerical stability)
+        C2i_0_svd = []
+        for c in C2i_0:
+            try:
+                c_svd = sl.svd(c, full_matrices=True)
+            except sl.LinAlgError:
+                # GESVD is more numerically stable, but slower
+                c_svd = sl.svd(c, full_matrices=True, lapack_driver='gesvd')
+
+            C2i_0_svd.append(c_svd)
+
+        # Select only non-singular values
+        C2i_sqrt_sing = [np.array([(np.sqrt(sv) if np.abs(sv) > 1e-10 else 0.0) for sv in s[1]]) for s in C2i_0_svd]          # Singular values -- 1D arrays
+        L_0 = [svd[0] @ np.diag(s) @ svd[0].T for (svd, s) in zip(C2i_0_svd, C2i_sqrt_sing)]          # L matrix -- 2D arrays
+
+        # Transformation 3:
+        # From now also construct the filter transform, because it is of manaeable size
+        LGNr = [l @ gnr for (l, gnr) in zip(L_0, self.GTNr)]          # List of 1D arrays (transformed data)
+        S3 = [l_bi @ r for (l_bi, r) in zip(L_0, self.R)]             # List of 2D arrays (Q transformer)
+
+        # Slice BigPhiDiff, because we only want non-zero items! 
+        PhiDiff = BigPhiDiff[self.par_msk, :][:, self.par_msk]
+
+        # A = L_B^{-1} G^T_T T^{prime} = L_B^{-1} @ R
+        # T^{prime} = L_N^{-1} Tmat
+        # P_T T^{prime} = T^{prime}   ===> P_T = G_T G_T^T
+        # S3m = S3 @ G_F (is same thing as selecting the columns of S3)
+        S3m = [s3[:, msk] for (msk, s3) in zip(self.par_psr_msk, S3)]          # S3 matrix with only the relevant columns
+
+        # Need to swap the projector S3m = S3 @ G_F = P_A @ S3 @ G_F = U_A U_A^T
+        U_A = [sl.svd(s3m, full_matrices=False)[0][:, :s3m.shape[1]] for s3m in S3m]
+
+        # Transformation 4:
+        # So now the data is:
+        ULGNr = [ua.T @ lgnr for (ua, lgnr) in zip(U_A, LGNr)]
+        S4 = [ua.T @ s3m for (ua, s3m) in zip(U_A, S3m)]
+
+        # For testing, we could also use different coordinates:
+        chi = ULGNr                         # Whitened data
+        chi_tot = np.concatenate(chi)       # ''
+        S = S4                              # Q transform
+        Phi = PhiDiff                       # H1-H0 difference
+
+        # build the list of block‐sizes and cumulative indices
+        block_sizes = [s.shape[0] for s in S]
+        idx = np.cumsum([0] + block_sizes)
+
+        npsrs = len(block_sizes)
+
+        # slice PhiDiff into npsrs×npsrs little blocks
+        Phi_blocks = [
+            [
+                Phi[idx[i] : idx[i + 1], idx[j] : idx[j + 1]]
+                for j in range(npsrs)
+            ]
+            for i in range(npsrs)
+        ]
+
+        # numerator (inner product) of os
+        # denominator (trace) of os
+        num = 0.0
+        den2 = 0.0
+        ddmat = np.zeros((npsrs, npsrs))
+        Q = np.zeros_like(Phi)
+        for i, (Si) in enumerate(S):   # The error is likely here
+            for j, (Sj) in enumerate(S):
+                SPS = Si @ Phi_blocks[i][j] @ Sj.T
+                Q[idx[i] : idx[i + 1], idx[j] : idx[j + 1]] = SPS
+
+                num += chi[i].dot(SPS @ chi[j])
+                ddmat[i, j] = np.trace(SPS @ SPS.T)
+                den2 += ddmat[i, j]
+                
+        den = np.sqrt(2*den2)   # Factor of 2 because of *real* (not complex) data
+        Q = Q / den
+
+        return num/den, chi_tot, Q
+
+    def compute_os(self, params):
+        os, _, _ = self._get_compressed_coordinates(params)
+        return os
+
+    def get_fixedpar_os_distribution(self, params, ds_min=-5, ds_max=20, cutoff=1e-6, limit=100, epsabs=1e-6):
+        """For given parameters, get the OS distribution"""
+        _, _, Q = self._get_compressed_coordinates(params)
+        eigen_values = sl.eigvalsh(Q)
+
+        xs = np.linspace(ds_min, ds_max, 1000)
+
+        # pdf = gx2pdf(eigen_values, xs, cutoff=cutoff, limit=limit, epsabs=epsabs)
+        cdf = gx2cdf(eigen_values, xs, cutoff=cutoff, limit=limit, epsabs=epsabs)
+
+        return xs, cdf
+
+
+    def get_fixedpar_pval(self, params, cutoff=1e-6, limit=100, epsabs=1e-6):
+        os, _, Q = self._get_compressed_coordinates(params)
+        eigen_values = sl.eigvalsh(Q)
+        cdf_val = gx2cdf(eigen_values, [os], cutoff=cutoff, limit=limit, epsabs=epsabs)[0]
+
+        return 1-cdf_val
